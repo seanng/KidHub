1509e27ae905d89a7d403582320ea0bd8a18bbe0
seanng <shonum@gmail.com>
2016-03-28 22:01:25 +0800

    set up feed html template

--- scripts/feed.js
+++ scripts/feed.js
@@ -1,7 +1,23 @@
 if (Meteor.isClient) {
   angular.module('KidHubApp')
   .controller('FeedCtrl', ['$scope','$meteor', function($scope, $meteor){
-    $scope.tester = "this is the scope";
+    $scope.timeslots = [{
+      activity_id: 543,
+      time: '10:45am',
+      duration: '45',
+      tokens: 4,
+      activity_name: "Junior Muay Thai",
+      district: 'Kennedy Town',
+      ages: '2-10'
+    },{
+      activity_id: 12345,
+      time: '11:45am',
+      duration: '30',
+      tokens: 2,
+      activity_name: "Arm Wrestling",
+      district: 'New Territories',
+      ages: '10+'
+    }];
   }]);
 }
 
