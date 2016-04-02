41f0a9ccb9d83a64097993f1d652be97c46c2a27
seanng <shonum@gmail.com>
2016-04-03 00:23:49 +0800

    trying to get timeslot filtering to work, and started to work on the map markers

--- scripts/activity.js
+++ scripts/activity.js
@@ -7,26 +7,38 @@ if (Meteor.isClient) {
     if ($stateParams.activityId.length === 24) {
       var activityID = new ObjectID($stateParams.activityId);
       $scope.activity = Activities.findOne({_id: activityID});
+      $scope.activities = Activities.find().fetch();
       $scope.ageLow = Array.min($scope.activity.ages);
       $scope.ageHigh = Array.max($scope.activity.ages);
-
-      $scope.eventDates = [];
+      $scope.uniqueDates = {};
       $scope.selectedDate = null;
-
       var allTimeslots = Timeslots.find({activity_id: activityID}, {sort: {date: 1}}).fetch();
 
       var getDates = function(){
         allTimeslots.forEach(function(elem){
-          var eventDate = moment(elem.date).format("dddd, DD MMMM");
+          var eventDate = moment(elem.date).format("dddd, DD MMMM").toString();
           var eventTime = moment(elem.date).format("ha");
-          $scope.eventDates.push({date: eventDate, time: eventTime});
+          if (!$scope.uniqueDates[eventDate]){
+            $scope.uniqueDates[eventDate] = [];
+          }
+          $scope.uniqueDates[eventDate].push(eventTime);
         });
       };
+
       getDates();
+
+      $scope.selectDate = function(value) {
+        console.log(value);
+        $scope.selectedDate = value;
+      };
 
       $scope.map = { center: { latitude: $scope.activity.placeLat, longitude: $scope.activity.placeLong}, zoom: 17};
       $scope.marker = {
+        events: {
+          click: function(marker) {
 
+          }
+        }
       };
 
     } else {
